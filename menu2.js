c09894594a028b0a0cf760f4a9134058c1a8bed3
ACEDOG2002 <asadeocio@gmail.com>
2025-12-09 14:09:46 -0800

    new

--- menu2.js
+++ menu2.js
@@ -2,7 +2,7 @@ document.getElementById("exitBtn").addEventListener("click", function () {
   window.location.href = "project.html";
 });
 document.getElementById("tinBtn").addEventListener("click", function () {
-  window.location.href = ".html";
+  window.location.href = "/team project/Prototype 2/Protostore.html";
 
 });document.getElementById("franklinBtn").addEventListener("click", function () {
   window.location.href = "MrFranklin.html";
