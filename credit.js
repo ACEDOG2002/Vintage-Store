d00ff824dd9817cd65f5617a94e6d35ee987e3b8
ACEDOG2002 <asadeocio@gmail.com>
2025-12-08 21:21:23 -0800

    new

---
+++ credit.js
@@ -0,0 +1,4 @@
+document.getElementById("backBtn").addEventListener("click", function () {
+
+  window.location.href = "project.html";
+});
---
+++ project.js
@@ -0,0 +1,3 @@
+document.getElementById("exitBtn").addEventListener("click", function () {
+  window.location.href = "credit.html";
+});
